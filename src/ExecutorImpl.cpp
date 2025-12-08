833cde11b8e2aa65f3bb448b15a49b28c5e9a1bf
huangjiapeng <1875423610@qq.com>
2025-12-08 19:28:04 +0800

    test: Pass move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -13,6 +13,22 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
+    for (const auto cmd : commands) {
+        if (cmd == 'M') {
+            if (pose.direction == 'E') {
+                ++pose.x;
+
+            } else if (pose.direction == 'W') {
+                --pose.x;
+
+            } else if (pose.direction == 'N') {
+                ++pose.y;
+
+            } else if (pose.direction == 'S') {
+                --pose.y;
+            }
+        }
+    }
 }
 
 Pose ExecutorImpl::QueryPose(void) const noexcept
