3599e8b29fadc1512ed62a4e218d6aaa72a48125
huangjiapeng <1875423610@qq.com>
2025-12-09 22:07:16 +0800

    feat: Add test cases for reverse command

---
+++ tests/ExecutorReverseTest.cpp
@@ -0,0 +1,99 @@
+#include <gtest/gtest.h>
+
+#include "Executor.hpp"
+#include "PoseEq.hpp"
+
+namespace adas
+{
+TEST(ExecutorReverseTest, should_return_x_minus_1_given_status_is_reverse_command_is_M_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BM");
+
+    // then
+    const Pose target_pose{-1, 0, 'E'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+TEST(ExecutorReverseTest, should_return_facing_S_given_status_is_reverse_command_is_L_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BL");
+
+    // then
+    const Pose target_pose{0, 0, 'S'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+TEST(ExecutorReverseTest, should_return_facing_N_given_status_is_reverse_command_is_R_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BR");
+
+    // then
+    const Pose target_pose{0, 0, 'N'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+TEST(ExecutorReverseTest, should_return_x_minus_2_given_status_is_reversefast_command_is_M_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BFM");
+
+    // then
+    const Pose target_pose{-2, 0, 'E'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+TEST(ExecutorReverseTest, should_return_facing_S_and_x_minus_1_given_status_is_reversefast_command_is_L_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BFL");
+
+    // then
+    const Pose target_pose{-1, 0, 'S'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+TEST(ExecutorReverseTest, should_return_facing_N_and_x_minus_1_given_status_is_reversefast_command_is_R_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BFR");
+
+    // then
+    const Pose target_pose{-1, 0, 'N'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+TEST(ExecutorReverseTest, should_return_y_plus_1_given_command_is_BBM_and_facing_is_N)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::New({0, 0, 'N'}));
+
+    // when
+    executor->Execute("BBM");
+
+    // then
+    const Pose target_pose{0, 1, 'N'};
+
+    ASSERT_EQ(target_pose, executor->QueryPose());
+}
+}  // namespace adas
